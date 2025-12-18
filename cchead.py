ef1e2df871d8ee0eced7f390b1f470801443bdd4
moueslati <eng.oueslati.mohamed@gmail.com>
2025-12-18 08:59:04 +0100

    Implement step 3 of the challenge for handling multiple input files

--- cchead.py
+++ cchead.py
@@ -1,26 +1,27 @@
 import argparse
 import sys
+from typing import Dict
 
 
 def read_file(filename):
     """ Read a file"""
-    with open(filename, 'rb') as f:
+    with open(filename, 'r') as f:
         return f.read()
     
-def get_output_lines(input_lines, content_byte) -> str:
+def get_output_lines(input_lines, content_str) -> str:
     """Get the output of the first number of lines of the content"""
     output_content = ''
-    content_str = content_byte.decode('utf-8').strip().split('\n')
-    num_lines = min(input_lines, len(content_str)) 
-    for i, line in enumerate(content_str[:num_lines]):
+    content = content_str.strip().split('\n')
+    num_lines = min(input_lines, len(content)) 
+    for i, line in enumerate(content[:num_lines]):
         output_content += line + '\n' if i < num_lines-1 else line
 
     return output_content
 
-def get_output_bytes(input_bytes, content_byte) -> str:
+def get_output_bytes(input_bytes, content_str) -> str:
     """Get the output of the first number of bytes of the content"""
-    num_bytes = min(input_bytes, len(content_byte))
-    return content_byte[:num_bytes].decode('utf-8')
+    num_bytes = min(input_bytes, len(content_str))
+    return content_str[:num_bytes]
     
 def main():
     """Main flow"""
@@ -28,7 +29,7 @@ def main():
         description="cchead command"
     )
 
-    parser.add_argument('filename', nargs='?', default=None,
+    parser.add_argument('filenames', nargs='*', default=None,
                         help="file to read")
     parser.add_argument('-n', '--lines', type=int, default=10,
                         help="Display the first n lines")
@@ -38,25 +39,32 @@ def main():
     args = parser.parse_args()
 
     try:
-        content_byte = b''
-        if args.filename:
-            content_byte = read_file(args.filename)
+        content_dict:Dict[str, str] = {}
+        if args.filenames:
+            for f in args.filenames:
+                content_dict[f] = read_file(f)
         else:
-            content_byte = sys.stdin.buffer.read()
+            content_dict["stdin"] = sys.stdin.buffer.read().decode('utf-8')
     except FileNotFoundError:
-        print(f'file {args.filename} does not exist', file=sys.stderr)
+        print(f'file {f} does not exist', file=sys.stderr)
         exit(1)
     except PermissionError:
-        print(f'You are not permitted to access this file {args.filename}', file=sys.stderr)
+        print(f'You are not permitted to access this file {f}', file=sys.stderr)
         exit(1) 
 
     output_content = ''
-    
-    
+        
     if args.bytes:
-        output_content = get_output_bytes(args.bytes, content_byte)
+        for name, content in content_dict.items():
+            if name != "stdin" and len(content_dict) > 1:
+                output_content += f"==> {name} <==\n"
+            output_content += get_output_bytes(args.bytes, content) + "\n"
+        
     elif args.lines:
-        output_content = get_output_lines(args.lines, content_byte)
+        for name, content in content_dict.items():
+            if name != "stdin" and len(content_dict) > 1:
+                output_content += f"==> {name} <==\n"
+            output_content += get_output_lines(args.lines, content) + "\n"
 
     print(output_content)
 
